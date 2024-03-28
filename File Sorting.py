bc32d9436e4feb45f32e751084cb16773855d957
Jaewon Shim <161691472+JaewonGithub@users.noreply.github.com>
2024-03-28 11:47:50 +0900

    Add files via upload

---
+++ File Sorting.py
@@ -0,0 +1,55 @@
+# %%
+import os, shutil
+
+# %%
+#The path of the file that needs to be sorted
+path = r"C:/Users/wodnj/Desktop/FileSortingTest/"
+
+# %%
+#Checks for the list of the files in the path folder that we will be sorting within
+os.listdir(path)
+
+# %%
+#Create folder for each different categories of files if not already existing
+#Here, since there are 7 categories of the files I would like to sort,
+#I have created seven different folders for them since they don't already exist.
+
+folder_names = ['xlsx files' , 'jpg files' , 'docx files', 'png files', 'csv files', 'txt files', 'pdf files']
+
+for count in range(7):
+    if not os.path.exists(path + folder_names[count]):
+        os.makedirs(path + folder_names[count])
+        print (path + folder_names[count])
+    
+
+# %%
+#For each of the file, use shutil library to move the file to the corresponding categorized folder
+#Here, All different types of files including xlsx,jpg,docx,png,csv,txt,pdf are sorted into their 
+#corresponding folders.
+
+list_of_files = os.listdir(path)
+
+for file in list_of_files :
+    if ".xlsx" in file and not os.path.exists(path + "xlsx files/" + file):
+        shutil.move( path + file , path + "xlsx files/" + file )
+    elif ".jpg" in file and not os.path.exists(path + "jpg files/" + file):
+        shutil.move( path + file , path + "jpg files/" + file )
+    elif ".docx" in file and not os.path.exists(path + "docx files/" + file):
+        shutil.move( path + file , path + "docx files/" + file )
+    elif ".docx" in file and not os.path.exists(path + "png files/" + file):
+        shutil.move( path + file , path + "png files/" + file )
+    elif ".docx" in file and not os.path.exists(path + "csv files/" + file):
+        shutil.move( path + file , path + "csv files/" + file )
+    elif ".docx" in file and not os.path.exists(path + "txt files/" + file):
+        shutil.move( path + file , path + "txt files/" + file )
+    elif ".docx" in file and not os.path.exists(path + "pdf files/" + file):
+        shutil.move( path + file , path + "pdf files/" + file )
+    else:
+        print("Not supported type of file")
+
+# %%
+#Checks to make sure if any type of file is still not relocated
+#Since the output is all folders, we have successfully relocated all of our files to corresponding folders.
+os.listdir(path)
+
+
